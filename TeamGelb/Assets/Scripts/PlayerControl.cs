88f4d09b7bbd7cadf29aa77196bcfa80451d0cea
gianni <58817136+GianniGithub@users.noreply.github.com>
2023-11-11 14:55:25 +0100

    AddPrefap

--- TeamGelb/Assets/Scripts/PlayerControl.cs
+++ TeamGelb/Assets/Scripts/PlayerControl.cs
@@ -11,7 +11,7 @@ public class PlayerControl : MonoBehaviour
     
     public Vector3 forceDirection;
     public float StepAngel;
-
+    public float StepSpeed;
     private float angel;
     
     private Rigidbody rb;
@@ -40,6 +40,16 @@ public class PlayerControl : MonoBehaviour
             case -1f:
                 angel -= StepAngel;
                 forceDirection = new Vector3(Mathf.Sin(Mathf.Deg2Rad * angel), 0f, Mathf.Cos(Mathf.Deg2Rad * angel));
+                break;
+        
+        }
+        switch (input.GetForwardDelta())
+        {
+            case 1f:
+                Speed += StepSpeed;
+                break;
+            case -1f:
+                angel -= StepSpeed;
                 break;
         
         }
